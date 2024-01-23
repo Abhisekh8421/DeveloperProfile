14c2169bbba65d89502551ce020f4da6f975849a
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-01-23 21:50:29 +0530

    some changes are made

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ dotenv.config({
 const app = express();
 //middlewares
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "15kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(path.resolve(), "public")));
 
-console.log(path.join(path.resolve(), "public"));
+// console.log(path.join(path.resolve(), "public")); for checking
 app.set("view engine", "ejs");
 
 //router
